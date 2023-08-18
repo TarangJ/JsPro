a8a03346b250aad11448bce24f8e91228a7d08ff
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2023-08-18 12:11:21 +0530

    Add files via upload

---
+++ DarkMode/data.js
@@ -0,0 +1,30 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Started learning JS',
+    date: new Date(2023,5 , 2),
+    length: 11,
+    snippet: `Self motivated.`,
+  },
+  {
+    id: 2,
+    title: 'Hi',
+    date: new Date(2023, 8, 1),
+    length: 5,
+    snippet: `Hi.`,
+  },
+  {
+    id: 3,
+    title: 'Failures are not losers',
+    date: new Date(2023, 8, 3),
+    length: 5,
+    snippet: `True.`,
+  },
+  {
+    id: 4,
+    title: 'keep trying',
+    date: new Date(2023, 8, 6),
+    length: 5,
+    snippet: `Agreed.`,
+  },
+];
